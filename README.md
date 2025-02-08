98e85ab36e4320ade6f4d450907478df6389d30f
prince <savaliyaprincem@gmail.com>
2025-02-08 21:18:08 +0530

    snack-game

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Analysis of Data Structures 
+## **Analysis of Data Structures** 
 
 In the context of the snake game, multiple data structures are employed to facilitate the functioning of the game grid, the movement of the snake, the positioning of food, and the general state of the game. Let us review some of the important data structures that are present in the code. 
 
@@ -12,15 +12,15 @@ The game grid is treated as a representation, it is not a data structure on its 
 
 ### 2. **Snake Representation** 
 
-The snake is constructed from a **vector of pairs**, more specifically a `vector<pair<int, int>> snake;`. 
+The snake is constructed from a *vector of pairs*, more specifically a vector<pair<int, int>> snake;. 
 
 - Each pair corresponds to one segment of the snake’s body in the grid. 
 
-- The first element from the pair represents the **x-coordinate** (horizontal position) of the snake segment. 
+- The first element from the pair represents the *x-coordinate* (horizontal position) of the snake segment. 
 
-- The second element from the pair represents the **y-coordinate** (vertical position) of the snake segment.
+- The second element from the pair represents the *y-coordinate* (vertical position) of the snake segment.
 
-Each time a snake consumes food, its body elongates by adding new segments at the tail of the snake with the command `snake.push_back({frontX, frontY})`. When the player inputs a command to move the snake, the head of the snake shifts toward the direction of the command, while the rear neck part of the snake gets detached to create the illusion of movement with the command `snake.erase(snake.begin())`.
+Each time a snake consumes food, its body elongates by adding new segments at the tail of the snake with the command snake.push_back({frontX, frontY}). When the player inputs a command to move the snake, the head of the snake shifts toward the direction of the command, while the rear neck part of the snake gets detached to create the illusion of movement with the command snake.erase(snake.begin()).
 
 
 
@@ -28,30 +28,30 @@ Each time a snake consumes food, its body elongates by adding new segments at th
 
 ### 3. **Food Representation**  
 
-The actual food’s position on the grid is specified with the help of two variables, `foodX` and `foodY`, which indicate the food's placement on the grid and type. The `foodtype` variable is given the task of determining if the food is of normal type (denoted by "$") or a special type (denoted by "%").  
+The actual food’s position on the grid is specified with the help of two variables, foodX and foodY, which indicate the food's placement on the grid and type. The foodtype variable is given the task of determining if the food is of normal type (denoted by "$") or a special type (denoted by "%").  
 
-- The snake head collides with the food (`frontX == foodX && frontY == foodY`) which results in the snake consuming it with both the head and tail. This elevates its length by one segment.    
+- The snake head collides with the food (frontX == foodX && frontY == foodY) which results in the snake consuming it with both the head and tail. This elevates its length by one segment.    
 
-- These are performed respectively in random order. The `foodPlace()` function repositions the food to a new location where it does not intersect or overlap with any part of the snakes body.  <br>  
+- These are performed respectively in random order. The foodPlace() function repositions the food to a new location where it does not intersect or overlap with any part of the snakes body.   
 
 ### 4. **Game State**  
 
 These states of the game are controlled with multiple integer and boolean variables: 
 
-- **gameRunning**: This is a boolean statement that is true by default and will only change to false if the snake hits a wall or if it collides with itself.  
+- *gameRunning*: This is a boolean statement that is true by default and will only change to false if the snake hits a wall or if it collides with itself.  
 
-- **score**: An integer that is increased every time the player gets a certain amount of points. So, consumption of food by the snake will increase the score and in cases of special food, boost it even more. 
+- *score*: An integer that is increased every time the player gets a certain amount of points. So, consumption of food by the snake will increase the score and in cases of special food, boost it even more. 
 
-- **highscore**: This integer memory stores the current highest amount of points achieved by the player in the present game state session. 
+- *highscore*: This integer memory stores the current highest amount of points achieved by the player in the present game state session. 
 
 ### 5. **Direction of Movement**
 
 The variable ‘direction’ determines the movement of the snake and an integer value is updated according to user input. 
 
-- ‘0’ means **upwards**. 
-- ‘1’ means **rightwards**. 
-- ‘2’ means **downwards**. 
-- ‘3’ means **leftwards**. 
+- ‘0’ means *upwards*. 
+- ‘1’ means *rightwards*. 
+- ‘2’ means *downwards*. 
+- ‘3’ means *leftwards*. 
 
 Players utilize the arrow keys on the keyboard or ‘W’, ‘A’, ‘S’, ‘D’ to update the direction. The snake may only make a turn if it does not move straight into the body of the snake.
 
@@ -79,5 +79,3 @@ In this case the structures of data are not complicated at all, but they are pro
 The ease of understanding the data structures, and the basic control flow framework which include, but are not limited to the vector for the snake as well as the mouse make the game easy to play and fast acting.
 
 Nonetheless, the addition of power-ups, levels or diverse food types can be included by introducing new data structures as well as modifying the existing ones.
-
-
---
+++ final.cpp
@@ -0,0 +1,231 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+using namespace std ;
+int foodtype = 0 ;
+
+
+string playerName;
+int se ;
+int highscore ;
+
+class SnakeGame {
+private:
+    int gridWidth;
+    int gridHeight;
+    int frontX, frontY;
+    int foodX, foodY;
+    vector<pair<int, int>> snake ;
+    int direction;
+    bool gameRunning;
+    int score;
+
+public:
+    SnakeGame() : gridWidth(se), gridHeight(se) 
+    {
+        initialize();
+    }
+
+    void initialize() {
+        frontX = gridWidth / 2;
+        frontY = gridHeight / 2;
+        snake.clear();
+        snake.push_back({frontX, frontY});
+        direction = 1;  
+        gameRunning = true;
+        score = 0;
+        foodPlace();
+    }
+
+    void draw() {
+        system("cls") ;
+        for (int i = 0; i < gridHeight; i++) 
+        {
+            for (int j = 0; j < gridWidth; j++) 
+            {
+                if(i == 0 || i == gridHeight - 1 || j == 0 || j == gridWidth - 1)
+                {
+                    cout << "#" ;  
+                } 
+                else if (i == frontY && j == frontX) 
+                {
+                    cout << "@" ; 
+                } 
+                else if (i == foodY && j == foodX) 
+                {
+                    if(foodtype==0)cout << "$" ;
+                    else  cout<<"%" ; 
+                } 
+                else 
+                {
+                    bool isBody = false;
+                    for (auto segment : snake) 
+                    {
+                        if (segment.first == j && segment.second == i) 
+                        {
+                            cout << "*";  
+                            isBody = true;
+                            break;
+                        }
+                    }
+                    if (!isBody) cout << " ";
+                }
+            }
+            cout << endl;
+        }
+        cout << "Score: " << score << endl;
+    }
+
+    void input() 
+    {
+        if (_kbhit()) 
+        {
+            int key = _getch();
+
+            if (key == 224) 
+            {
+                key = _getch();
+                switch (key) {
+                    case 72: if (direction != 2) direction = 0; break;  
+                    case 77: if (direction != 3) direction = 1; break;
+                    case 80: if (direction != 0) direction = 2; break;  
+                    case 75: if (direction != 1) direction = 3; break;  
+                }
+            } else 
+            {
+                switch (key) 
+                {
+                    case 'w': if (direction != 2) direction = 0; break;  
+                    case 'd': if (direction != 3) direction = 1; break;  
+                    case 's': if (direction != 0) direction = 2; break;  
+                    case 'a': if (direction != 1) direction = 3; break;  
+                }
+            }
+        }
+    }
+
+    void update() 
+    {
+        switch (direction) 
+        {
+            case 0: frontY--; break;  
+            case 1: frontX++; break;  
+            case 2: frontY++; break;  
+            case 3: frontX--; break;   
+        }
+
+        if (frontX <= 0 || frontX >= gridWidth - 1 || frontY <= 0 || frontY >= gridHeight - 1) 
+        {
+            gameRunning = false;
+            return;
+        }
+
+        for (auto segment : snake) 
+        {
+            if (segment.first == frontX && segment.second == frontY) 
+            {
+                gameRunning = false;
+                return;
+            }
+        }
+
+        bool chefood = false ;
+        if (frontX == foodX && frontY == foodY) 
+        {
+            if(foodtype==0) score += 1 ;
+            else score += 5 ;
+            snake.push_back({foodX, foodY});
+            foodPlace();
+            chefood = true ;
+        }
+
+        if(!chefood) 
+        {
+            snake.push_back({frontX,frontY}) ;
+            snake.erase(snake.begin()) ;
+        }
+    }
+
+    void foodPlace() {
+        srand(time(0));
+        while (true) {
+            foodX = rand() % (gridWidth - 2) + 1;
+            foodY = rand() % (gridHeight - 2) + 1;
+
+            bool onSnake = false;
+            for (auto segment : snake) {
+                if (segment.first == foodX && segment.second == foodY) {
+                    onSnake = true;
+                    break;
+                }
+            }
+            if (!onSnake) break;
+        }
+        foodtype = rand()%2 ;
+    }
+
+    void run() 
+    {
+        while (true) {
+            gameRunning = true;
+            cout << "Press 'h' for hard mode, 'm' for medium mode, 'e' for easy mode" << endl;
+            char chh;
+            cin >> chh;
+
+            int an;
+            if (chh == 'h') an = 50;  
+            else if (chh == 'm') an = 100; 
+            else an = 200; 
+
+            initialize();
+            while (gameRunning)
+            {
+                draw();
+                input();
+                update();
+                _sleep(an);
+            }
+            if(score>highscore) highscore = score ;
+            cout << "Game Over! " <<endl<< playerName << "'s score: " << score << endl ;
+            cout<<"->  your highest score: "<<highscore<<endl ;
+            cout << "-> Press 'i' to play again or any other key to exit." << endl;
+            char ch;
+            cin >> ch;
+
+            if (ch != 'i') break;  
+        }
+    }
+};
+
+int main() {
+    cout << "Enter Player name: " << endl;
+        cin >> playerName;
+        cout<<endl ;
+        cout << "Hello " << playerName << endl;
+        cout << endl;
+        cout << endl;
+        cout << "WELCOME TO SNAKE GAME" << endl;
+        cout << endl;
+        cout << "GAME RULES :" << endl;
+        cout << endl;
+        cout << "-> There will be one grid in which your snake will move." << endl;
+        cout << "-> If your snake collides with the wall or its body part, the game will be over." << endl;
+        cout << "-> Press 'w' or the up arrow for moving the snake upward." << endl;
+        cout << "-> Press 'd' or the right arrow for moving the snake right." << endl;
+        cout << "-> Press 's' or the down arrow for moving the snake downward." << endl;
+        cout << "-> Press 'a' or the left arrow for moving the snake left." << endl;
+        cout << endl;
+        cout<<endl ;
+        cout<<"select grid size as you want"<<endl ;
+        cout<<"->  Press 1 for small grid"<<endl ;
+        cout<<"->  Press 2 for medium grid"<<endl ;
+        cout<<"->  Press 3 for big grid"<<endl ;
+        int gg ;
+        cin>>gg ;
+        if(gg==1) se = 15 ;
+        if(gg==2) se = 20 ;
+        if(gg==3) se = 30 ;
+        else se = 15 ;
+    SnakeGame game;
+    game.run();
+    return 0;
+}
